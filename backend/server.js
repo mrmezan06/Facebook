b1bd633eb6de7ae07d0dc491bc72d73a6bc691bd
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-25 16:39:52 +0600

    MongoDB connection established

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-dotenv.config();
 const { readdirSync } = require("fs");
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
 
 /* const userRoutes = require("./routes/user");
@@ -13,8 +13,18 @@ app.use(cors());
 app.use("/user", userRoutes); */
 
 /* Dynamically adding routes localhost:8000/Routes_File_Name */
-
+// Routes
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
+
+// Database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((err) => console.log("Error connection - ", err));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
