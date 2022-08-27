4eb8c348c5e5b0af8a31045fd30fc8a9cb67c18d
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-27 15:24:44 +0600

    Account Activation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,6 +4,7 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/token");
 const { sendVerificationEmail } = require("../helpers/mailer");
+const jwt = require("jsonwebtoken");
 
 exports.register = async (req, res) => {
   try {
@@ -95,5 +96,21 @@ exports.register = async (req, res) => {
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+  const check = await User.findById({ _id: user.id });
+  if (check.verified) {
+    return res.status(400).json({
+      message: "Account already activated!",
+    });
+  } else {
+    await User.findByIdAndUpdate({ _id: user.id }, { verified: true });
+    return res.status(200).json({
+      message: "Account has been activated successfully!",
+    });
   }
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register } = require("../controllers/user");
+const { register, activateAccount } = require("../controllers/user");
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/activate", activateAccount);
 
 module.exports = router;
