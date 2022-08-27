9123f1ccdc45ffc6432b2ed85e91f112ff1ff951
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-27 18:14:03 +0600

    Front End Basic Structure defined

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
+import Home from "./pages/home";
+
 function App() {
-  const get = async () => {
-    const response = await fetch("http://localhost:8000/");
-
-    console.log(response);
-  };
-
-  get();
-  return <div>Welcome to frontend</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<Login />} exact />
+        <Route path="/profile" element={<Profile />} exact />
+        <Route path="/" element={<Home />} exact />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById("root")
 );
