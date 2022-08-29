433664765c67b619383dd82bff86add63ba18b11
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-08-30 00:30:27 +0600

    Register header completed

--- frontend/src/components/login/RegisterForm.js
+++ frontend/src/components/login/RegisterForm.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  return (
+    <div className="blur">
+      <div className="register">
+        <div className="register_header">
+          <i className="exit_icon"></i>
+          <span>Sign up</span>
+          <span>It's quick and easy</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RegisterForm;
